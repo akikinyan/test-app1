0be0d6fc79eb0ed56fb0e4dd9a8ef26e70716033
akira.kitama <akikinyan@gmail.com>
2020-04-30 14:30:04 +0900

    Updates

---
+++ precache-manifest.81e952a69ea85ba14ca95d2d4d164f43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6255ee1ddc4767ef4a53c4ccf524e4",
+    "url": "/test-app1/index.html"
+  },
+  {
+    "revision": "d2e14efe28abf65d3728",
+    "url": "/test-app1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fefe7b19c718aac80f03",
+    "url": "/test-app1/static/js/2.dc51c2fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-app1/static/js/2.dc51c2fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e14efe28abf65d3728",
+    "url": "/test-app1/static/js/main.49fff4f2.chunk.js"
+  },
+  {
+    "revision": "00fe22a9f7de615f06f2",
+    "url": "/test-app1/static/js/runtime-main.05d410b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app1/static/media/logo.5d5d9eef.svg"
+  }
+]);
